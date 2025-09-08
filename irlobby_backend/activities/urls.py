5636bdfcb394c2a415d8115b00ae65526294350e
Aaron <ajesbenshade@outlook.com>
2025-09-08 16:52:35 -0400

    Move populate endpoint to activities app to bypass users URL loading issue

--- irlobby_backend/activities/urls.py
+++ irlobby_backend/activities/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from users.views import populate_test_data_api
 
 urlpatterns = [
     path('', views.ActivityListCreateView.as_view(), name='activity-list'),
@@ -10,4 +11,6 @@ urlpatterns = [
     path('<int:pk>/chat/', views.activity_chat, name='activity-chat'),
     path('<int:pk>/swipe/', views.swipe_activity, name='swipe-activity'),
     path('discover/', views.discover_activities, name='discover-activities'),
+    # Temporary populate endpoint
+    path('populate-test-data/', populate_test_data_api, name='populate-test-data'),
 ]

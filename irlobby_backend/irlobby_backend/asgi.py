1cdcab74126b89988068acbfe3467fb68a0c1c46
Aaron <ajesbenshade@outlook.com>
2025-09-08 07:34:52 -0400

    Fix ASGI configuration: configure Django settings before imports to prevent AppRegistryNotReady error

--- irlobby_backend/irlobby_backend/asgi.py
+++ irlobby_backend/irlobby_backend/asgi.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+# Configure Django settings before any Django imports
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'irlobby_backend.settings')
+
+# Setup Django
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'irlobby_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),

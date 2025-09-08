2a0fdf0b64c0716c2d6690d09815e69eedb86249
Aaron <ajesbenshade@outlook.com>
2025-09-08 16:26:37 -0400

    Remove unused render import

--- irlobby_backend/irlobby_backend/urls.py
+++ irlobby_backend/irlobby_backend/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,

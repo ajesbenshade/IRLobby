1a87a686603ab7c1dbc6b4b94d5e9cb88b7e6700
Aaron <ajesbenshade@outlook.com>
2025-09-18 17:32:20 -0400

    Revert "iOS Safari safe-area: move buffer to top, keep bottom nav flush; add pt-safe container and adjust viewport meta"
    This reverts commit 0fbecfda0eec5d06dae5b15f6a7285707c5c13b9.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,9 +64,7 @@ function App() {
         <Toaster />
         <Router>
           <Suspense fallback={<PageLoader />}>
-            <div className="min-h-screen pt-safe">
-              <AppRoutes />
-            </div>
+            <AppRoutes />
           </Suspense>
         </Router>
       </TooltipProvider>

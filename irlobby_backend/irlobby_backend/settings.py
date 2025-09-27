4c9ed541bd49fc0232e7bbe9a750746433c58f5e
Aaron <ajesbenshade@outlook.com>
2025-09-27 05:22:47 -0400

    Add password reset database fields and email configuration
    - Add password_reset_token and token_created_at fields to User model
    - Configure email settings for password reset functionality
    - Create database migration for new fields
    - Add test script for password reset endpoint

--- irlobby_backend/irlobby_backend/settings.py
+++ irlobby_backend/irlobby_backend/settings.py
@@ -239,3 +239,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@irlobby.com')
+
+# Frontend base URL for password reset links
+FRONTEND_BASE_URL = config('FRONTEND_BASE_URL', default='http://localhost:5173/')
